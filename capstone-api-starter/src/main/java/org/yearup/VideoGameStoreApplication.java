02b3f35fb48564059e7f3dfd780cd9cb7014c62a
FullStackMWilliams <markuswilliams99@icloud.com>
2025-12-14 19:22:43 -0500

    Adding to git repo/ adding rest controller for categories

---
+++ capstone-api-starter/src/main/java/org/yearup/VideoGameStoreApplication.java
@@ -0,0 +1,14 @@
+package org.yearup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoGameStoreApplication
+{
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoGameStoreApplication.class, args);
+    }
+
+}
