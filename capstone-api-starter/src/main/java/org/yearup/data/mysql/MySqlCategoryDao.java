3f0326bc7ad46a8843bf1a79199d15d80cbc5d3f
FullStackMWilliams <markuswilliams99@icloud.com>
2025-12-15 17:59:29 -0500

    Updated interface override method for update category

--- capstone-api-starter/src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ capstone-api-starter/src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -99,16 +99,30 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
 
 
     @Override
-    public void update(int categoryId, Category category)
-    {
-        // update category
+    public void update(int categoryId, Category category) {
+        String sql = "UPDATE categories SET name = ?, description = ? WHERE category_id = ?";
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setString(1, category.getName());
+            statement.setString(2, category.getDescription());
+            statement.setInt(3, categoryId);
+
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Error updating category id=" + categoryId, e);
+        }
     }
+
 
     @Override
     public void delete(int categoryId)
     {
         // delete category
     }
+
 
     private Category mapRow(ResultSet row) throws SQLException
     {
