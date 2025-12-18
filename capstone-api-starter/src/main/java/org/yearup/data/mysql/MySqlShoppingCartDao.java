ebea64c7474e62ee052e3e9810a2e1b1f2429342
FullStackMWilliams <markuswilliams99@icloud.com>
2025-12-18 12:40:23 -0500

    Updating MySqlShoppingCartDao class with rest methods

--- capstone-api-starter/src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
+++ capstone-api-starter/src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
@@ -1,4 +1,39 @@
 package org.yearup.data.mysql;
 
-public class MySqlShoppingCartDao {
+import org.springframework.stereotype.Repository;
+import org.yearup.data.ProductDao;
+import org.yearup.data.ShoppingCartDao;
+import org.yearup.models.ShoppingCart;
+import javax.sql.DataSource;
+
+
+@Repository
+public class MySqlShoppingCartDao extends MySqlDaoBase
+        implements ShoppingCartDao {
+    private ProductDao productDao;
+
+    public MySqlShoppingCartDao(DataSource dataSource, ProductDao productDao) {
+        super(dataSource);
+        this.productDao = productDao;
+    }
+
+    @Override
+    public ShoppingCart getByUserId(int userId) {
+        return null;
+    }
+
+    @Override
+    public void addProduct(int userId, int productId) {
+
+    }
+
+    @Override
+    public void updateProduct(int userId, int productId, int quantity) {
+
+    }
+
+    @Override
+    public void clearCart(int userId) {
+
+    }
 }
